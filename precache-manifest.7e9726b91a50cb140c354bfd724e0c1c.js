1ea3c12b6e84a08971d377496ac52f27b30fa522
Lucas Abud Rigonatto <lucas.rigonatto@gmail.com>
2024-10-30 08:53:37 -0300

    Updates

---
+++ precache-manifest.7e9726b91a50cb140c354bfd724e0c1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc486b54efc48aa9397894ac83b5b168",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/To-Do-List/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "454474e76f995d84406e",
+    "url": "/To-Do-List/static/js/main.85a466d0.chunk.js"
+  },
+  {
+    "revision": "1e5b64d60343e6eba72b",
+    "url": "/To-Do-List/static/js/runtime~main.2fdc73a5.js"
+  }
+]);
