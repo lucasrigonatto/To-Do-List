8ffbab64f46d0b827cacaeba433deed0541235e4
Lucas Abud Rigonatto <lucas.rigonatto@gmail.com>
2024-10-30 09:00:56 -0300

    Atualizações no projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home"; // Supondo que você tenha um componente Home
+import ToDoList from "./ToDoList";
+import NotFound from "./NotFound"; // Crie um componente NotFound para a página 404 (opcional)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/To-Do-List">
+      {" "}
+      {/* basename é o nome do seu repositório */}
+      <Routes>
+        <Route path="/" element={<ToDoList />} />
+        <Route path="*" element={<NotFound />} /> {/* Rota para 404 */}
+      </Routes>
+    </Router>
   );
 }
 
